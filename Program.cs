b2fec911aac049a793e79ccaf3d39f42ae1ad5d7
Ubuntu <ubuntu@ip-172-31-29-197.ec2.internal>
2025-05-13 06:09:27 +0000

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+// Ensure the app listens on all network interfaces
+app.Run("http://0.0.0.0:8080"); // <-- Add this line
+
+
